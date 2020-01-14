4de62b62d47aacd3f5145fa1f03e0adac78940fc
Henry Zhang <henryzhang9802@gmail.com>
2020-01-14 17:23:29 +0000

    Update info and methos

--- server.js
+++ server.js
@@ -12,8 +12,15 @@ const port = 8080
         res.send('POST request to the homepage')
     })
 
-    app.get('/json', function (req, res) {
+    app.post('/profile/update', function (req, res) {
+        res.send('Received')
+    })
+
+    app.get('/profile/get', function (req, res) {
         let obj = {
+            id: "uniqueID",
+            username: "jonmcnamara",
+            profile: "https://media-exp2.licdn.com/dms/image/C5603AQFA_oQhi6-2Cg/profile-displayphoto-shrink_800_800/0?e=1584576000&v=beta&t=QfVEJg5DU7IHXBiUlaZ2nRjI5gHTqok20eL17iHHa8Y",
             firstname : "John",
             lastname : "McNamara",
             description : "John is a Senior Inventor, Research Fellow, Impact Fellow and currently provides technical leadership for the IBM Hursley Innovation Centre. John has a diverse background that includes consultancy, performance, service & product delivery, all underpinned by a passion for innovation. Most recently his work leading the Innovation Centre technologist team has allowed him to combine these interests in order to maximise the potential of new technology while solving real problems. John has overseen the delivery of many cognitive cloud-based solutions and understands how to combine technologies to quickly provide value for customers. John is an active inventor with an invention portfolio spanning mobile, A.I, messaging, integration and predictive analytics.",
