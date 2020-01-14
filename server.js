2d84805b32683a31239121501f161069dc68cabf
Henry Zhang <henryzhang9802@gmail.com>
2020-01-14 17:01:05 +0000

    Add npm and express

--- server.js
+++ server.js
@@ -1,6 +1,25 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 8080
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Test for git auto deploy');
-}).listen(8080);
+// respond with "hello world" when a GET request is made to the homepage
+// GET method route
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage')
+  })
+  
+  // POST method route
+  app.post('/', function (req, res) {
+    res.send('POST request to the homepage')
+  })
+
+  app.get('/json', function (req, res) {
+    let obj = {
+        aaa: "bbb",
+        bbb: "ccc"
+    }
+
+    res.send(JSON.stringify(obj))
+  })
+
+  app.listen(port, () => console.log(`app listening on port ${port}!`))
