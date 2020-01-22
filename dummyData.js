2e0f4a4f4e5a7fc345c4d7afa8a3666a7f181f4c
Henry Zhang <henryzhang9802@gmail.com>
2020-01-22 17:27:55 +0000

    Update history to list

--- dummyData.js
+++ dummyData.js
@@ -58,7 +58,7 @@ module.exports = {
   getHistoryDummy: function(userid) {
     return {
       userid: userid ? userid : "dummy",
-      history: [
+      list: [
         generateHistoryProfile(),
         generateHistoryProfile(),
         generateHistoryProfile(),
