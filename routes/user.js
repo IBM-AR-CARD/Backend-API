3de43f3f99965357796d6a00fd1d89d3818fd562
Henry Zhang <henryzhang9802@gmail.com>
2020-01-30 22:06:26 +0000

    Bug fixes

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,14 @@ let dummy = require("../include/dummyData.js");
 
 router.post("/register", async function(req, res) {
   let newUser = {};
-  newUser.username = req.body.username;
-  newUser.email = req.body.email;
-  newUser.password = req.body.password;
+  console.log(req.body);
+  newUser.username = req.body.username
+    ? req.body.username
+    : returnError("Invalid", res);
+  newUser.email = req.body.email ? req.body.email : returnError("Invalid", res);
+  newUser.password = req.body.password
+    ? req.body.password
+    : returnError("Invalid", res);
 
   try {
     let findExisting = await db.dbFind("profiles", {
@@ -16,10 +21,7 @@ router.post("/register", async function(req, res) {
     });
 
     if (findExisting) {
-      res.status(500);
-      res.json({
-        error: "Username already exists"
-      });
+      returnError("Username already exists", res);
       return;
     }
 
@@ -28,11 +30,16 @@ router.post("/register", async function(req, res) {
     res.status(200);
     res.json({ success: "Successfully regisered, please now log in." });
   } catch (error) {
-    res.status(500);
-    res.json({
-      error: error
-    });
+    returnError(error, res);
   }
 });
+
+function returnError(error, res) {
+  res.status(500);
+  res.json({
+    error: error
+  });
+  res.end();
+}
 
 module.exports = router;
