2e2efd68e0718d5f555fb288a9d465c98ffff6d6
Henry Zhang <henryzhang9802@gmail.com>
2020-02-11 11:06:11 +0000

    Add watson message

--- include/watson.js
+++ include/watson.js
@@ -10,12 +10,54 @@ const assistant = new AssistantV2({
   disableSslVerification: true // TODO Turn off after ssl
 });
 
+const assistant_id = process.env.WATSON_ASSISTANT_ID;
+
 module.exports = {
-  createSession: function() {
+  createSession: async function() {
     return new Promise(function(resolve, reject) {
       assistant
         .createSession({
-          assistantId: process.env.WATSON_ASSISTANT_ID
+          assistantId: assistant_id
+        })
+        .then(res => {
+          console.log(JSON.stringify(res, null, 2));
+          resolve(res);
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+
+  deleteSession: async function(session_id) {
+    return new Promise(function(resolve, reject) {
+      assistant
+        .deleteSession({
+          assistantId: assistant_id,
+          sessionId: session_id
+        })
+        .then(res => {
+          console.log(JSON.stringify(res, null, 2));
+          resolve(res);
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  },
+
+  sendMessage: async function(session_id, message) {
+    return new Promise(function(resolve, reject) {
+      assistant
+        .message({
+          assistantId: assistant_id,
+          sessionId: session_id,
+          input: {
+            message_type: "text",
+            text: message
+          }
         })
         .then(res => {
           console.log(JSON.stringify(res, null, 2));
