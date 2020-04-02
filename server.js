cbcd6b8e4be49f78867de215be439ae2f5f115d0
Henry Zhang <henryzhang9802@gmail.com>
2020-04-02 17:41:03 +0100

    Add CORS

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 let bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(fileupload());
+app.use(cors());
 
 // Home page
 app.use(require("./routes/index"));
